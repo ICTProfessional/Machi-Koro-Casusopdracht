d4b145a64ca44241ca4fb08d6519bea626478544
ICTProfessional <66629997+Qusk2@users.noreply.github.com>
2022-01-15 17:13:14 +0100

    DoeEffect succesvol Abstract gemaakt in code

--- Machi Koro Casusopdracht/Backend/Kaarten/Gebouw.cs
+++ Machi Koro Casusopdracht/Backend/Kaarten/Gebouw.cs
@@ -6,9 +6,11 @@ using System.Threading.Tasks;
 
 namespace Machi_Koro_Casusopdracht
 {
-    class Gebouw : Kaart
+    abstract class Gebouw : Kaart
     {
         public List<int> Rolwaarden { get; set; }
         public override int BetrokkenMunten { get; set; }
+
+        public override abstract void DoeEffect();
     }
 }
--- Machi Koro Casusopdracht/Backend/Kaarten/Kaart.cs
+++ Machi Koro Casusopdracht/Backend/Kaarten/Kaart.cs
@@ -14,5 +14,7 @@ namespace Machi_Koro_Casusopdracht
         /* afbeelding moet nog als file variabele*/
         public int Icoon { get; set; }
         abstract public int BetrokkenMunten { get; set; }
+
+        abstract public void DoeEffect();
     }
 }
