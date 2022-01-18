33f52920ca9eaac065386e3455de0f0c926d98cd
ICTProfessional <2103747kleijnen@zuyd.nl>
2022-01-18 19:34:29 +0100

    Ding?

--- Machi Koro Casusopdracht/Frontend/Spelscherm.Designer.cs
+++ Machi Koro Casusopdracht/Frontend/Spelscherm.Designer.cs
@@ -143,7 +143,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1062, 450);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.img_Bezienswaardigheid4);
             this.Controls.Add(this.img_Bezienswaardigheid3);
