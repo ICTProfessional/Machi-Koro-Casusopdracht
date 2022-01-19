981d43b0b5e5400e37435507aafc6db0bb27093d
Golan Wolde <2103779wolde@zuyd.nl>
2022-01-19 20:12:13 +0100

    functies een beetje uitgewerkt

--- Machi Koro Casusopdracht/Backend/Speler.cs
+++ Machi Koro Casusopdracht/Backend/Speler.cs
@@ -13,5 +13,87 @@ namespace Machi_Koro_Casusopdracht
         public int AantalToernooiWins { get; set; }
         public List<Kaart> Gebouwen { get; set; }
         public List<Kaart> Bezienswaardigheden { get; set; }
+
+        public Speler(string _naam)
+        {
+            Naam = _naam;
+            ResetStats();
+        }
+
+        /*
+        public void KaartKopen(Kaart _kaart)
+        {
+            
+        } */
+
+        public void ResetStats()
+        {
+            Geld = 3;
+            /*Bezienswaardigheden.Clear();*/
+            Gebouwen.Clear();
+            Gebouwen.Add("Graanveld");
+        }
+        /// <summary>
+        /// Checkt of een speler een winkelcentrum heeft
+        /// </summary>
+        public bool HeeftWinkelCentrum()
+        {
+            
+            foreach (var bezienswaardigheidkaart in Bezienswaardigheden)
+            {
+                if (bezienswaardigheidkaart.Naam.ToLower() == "winkelcentrum")
+                {
+                    return bezienswaardigheidkaart.IsActief;
+                }
+            }
+            return false;
+        }
+        /// <summary>
+        /// Checkt of de speler een Station heeft.
+        /// </summary>
+        public bool HeeftStation()
+        {
+            foreach (var bezienswaardigheidkaart in Bezienswaardigheden)
+            {
+                if (bezienswaardigheidkaart.Naam.ToLower() == "station")
+                {
+                    return bezienswaardigheidkaart.IsActief;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Checkt of een speler een pretpark heeft
+        /// </summary>
+        public bool HeeftPretpark()
+        {
+            foreach (var bezienswaardigheidkaart in Bezienswaardigheden)
+            {
+                if (bezienswaardigheidkaart.Naam.ToLower() == "pretpark")
+                {
+                    return bezienswaardigheidkaart.IsActief;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Checkt of een spelere een radiotoren
+        /// </summary>
+        public bool HeeftRadioToren()
+        {
+            foreach (var bezienswaardigheidkaart in Bezienswaardigheden)
+            {
+                if (bezienswaardigheidkaart.Naam.ToLower() == "radiotoren")
+                {
+                    return bezienswaardigheidkaart.IsActief;
+                }
+            }
+            return false;
+        }
+
+
+
     }
 }
